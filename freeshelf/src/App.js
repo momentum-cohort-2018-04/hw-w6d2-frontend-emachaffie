651a25f7540ae3ae6ac6191067db1c8bafaffa98
emachaffie <emachaffie@gmail.com>
2018-05-30 14:41:43 -0400

    Added basic info and skeleton to app.js, expanded book info currently displayed

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Book from './Book'
+import books from './books.json'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      bookInfoExpanded: false,
+      books: books
+    }
+  }
+
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Read About Coding So You Can Read Code</h1>
+        {books.map((book, i) => (
+          <div key={i} className='bookDiv'>
+            <h3>{book.title}</h3>
+            <p>Author: {book.author}</p>
+            <p>About this book: {book.shortDescription}</p>
+            <img src={book.coverImageUrl} />
+            {/* wrap below in <a> tag? */}
+            <p className='readMoreOrLess'>Read more</p>
+            <div className='expandedBookDiv'>
+              <p><a href src={book.url} /></p>
+              <p>Publisher: {book.publisher}</p>
+              <p>Published {book.publicationDate}</p>
+              <p>In more detail: {book.detailedDescription}</p>
+            </div>
+          </div>
+        ))}
+
       </div>
     )
   }
